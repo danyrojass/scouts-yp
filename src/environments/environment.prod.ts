a1f1bdc29c524277bafd1eb9d3c73503ace505e9
Daniel Rojas <danyrsimon@hotmail.com>
2026-04-18 10:33:03 -0300

    refactor: remove hardcoded Firebase keys, prepare for Vercel deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
+// Production environment - values come from Vercel environment variables
 export const environment = {
     production: true,
     firebase: {
-        apiKey: "AIzaSyCTVok0KrV_S8l5E-LNvsHrlpHgkyNhrZs",
-        authDomain: "scout-app-7b912.firebaseapp.com",
-        projectId: "scout-app-7b912",
-        storageBucket: "scout-app-7b912.firebasestorage.app",
-        messagingSenderId: "823584633196",
-        appId: "1:823584633196:web:eba2da82f22701905e0349",
-        measurementId: "G-47XS2TVW2H"
+        apiKey: '',
+        authDomain: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: '',
+        measurementId: ''
     }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyCTVok0KrV_S8l5E-LNvsHrlpHgkyNhrZs",
-        authDomain: "scout-app-7b912.firebaseapp.com",
-        projectId: "scout-app-7b912",
-        storageBucket: "scout-app-7b912.firebasestorage.app",
-        messagingSenderId: "823584633196",
-        appId: "1:823584633196:web:eba2da82f22701905e0349",
-        measurementId: "G-47XS2TVW2H"
+        apiKey: 'demo-api-key',
+        authDomain: 'demo.firebaseapp.com',
+        projectId: 'demo-project',
+        storageBucket: 'demo.appspot.com',
+        messagingSenderId: '123456789',
+        appId: '1:123456789:web:demo',
+        measurementId: 'G-DEMO123'
     }
 };
