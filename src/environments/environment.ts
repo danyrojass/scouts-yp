bef6ae2d88e53559a7573717c7da25b42ac065f7
Daniel Rojas <danyrsimon@hotmail.com>
2026-04-18 12:28:17 -0300

    fix: remove sensitive data and use environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: 'demo-api-key',
-        authDomain: 'demo.firebaseapp.com',
-        projectId: 'demo-project',
-        storageBucket: 'demo.appspot.com',
-        messagingSenderId: '123456789',
-        appId: '1:123456789:web:demo',
-        measurementId: 'G-DEMO123'
+        apiKey: process.env['NG_APP_FIREBASE_API_KEY'] || "",
+        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || "",
+        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || "",
+        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || "",
+        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || "",
+        appId: process.env['NG_APP_FIREBASE_APP_ID'] || "",
+        measurementId: process.env['NG_APP_FIREBASE_MEASUREMENT_ID'] || ""
     }
 };
